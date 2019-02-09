317180e98ee2df26d3b7872b6243d9459a644a4b
kataatak05 <46829756+kataatak05@users.noreply.github.com>
2019-02-09 01:54:13 +0100

    Add files via upload

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := gnsz6753_66_n,A8,gionee6753_65u_m0
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
---
+++ board/telephony.mk
@@ -0,0 +1,3 @@
+# RIL
+BOARD_RIL_CLASS := ../../../device/condor/gnsz6753_66_n/ril
+BOARD_CONNECTIVITY_MODULE := conn_soc
